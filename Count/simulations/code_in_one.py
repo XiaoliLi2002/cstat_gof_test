feccf928e0d4bcb23a4d11b7cfa8054214945414
Xiaoli Li <121785218+XiaoliLi2002@users.noreply.github.com>
2024-11-06 23:57:29 -0600

    Update code_in_one.py

--- Count/simulations/code_in_one.py
+++ Count/simulations/code_in_one.py
@@ -2,8 +2,7 @@ import pandas as pd
 import numpy as np
 import math
 import statistics
-from scipy.stats import poisson
-from scipy.stats import nbinom
+from scipy.stats import poisson, norm, chi2, gamma
 import scipy.optimize as opt
 import scipy
 import time
@@ -50,26 +49,16 @@ def normfun(x,mu,sigma):
     return pdf
 
 def poisson_data(mu):
-    arr=np.zeros(len(mu))
-    for i in range(len(mu)):
-        arr[i]=np.random.poisson(mu[i])
-    return arr
+    return np.random.poisson(mu)
 
-def poisson_data_constant(mu,n):
-    return poisson.rvs(mu=mu,size=n)
 
 def nb_data(r,mu):
-    arr = np.zeros(len(mu))
-    prob = np.zeros(len(mu))
-    for i in range(len(mu)):
-        prob[i]=r/(r+mu[i])
-    for i in range(len(mu)):
-        arr[i] = nbinom.rvs(r,prob[i],size=1)[0]
-    return arr
+    prob = r/(r+mu)
+    return np.random.negative_binomial(r,prob)
 
 def p_value_norm(x,mu,sigma):
     z=(x-mu)/sigma
-    return scipy.stats.norm.sf(z)
+    return norm.sf(z)
 
 def generate_s_exp(n,beta):
     theta1=beta[0]
@@ -94,7 +83,7 @@ def generate_s_norm(n,mu,sigma):
     return np.random.normal(mu,sigma,n)
 
 def generate_s_gamma(n,alpha,beta):
-    return scipy.stats.gamma.rvs(alpha,loc=0,scale=1/beta,size=n)
+    return gamma.rvs(alpha,loc=0,scale=1/beta,size=n)
 
 def generate_s(n,beta,type):
     if type=='exp':
@@ -149,7 +138,7 @@ def Max_var(s):
     return var
 
 def poisson_dis(mu,i):
-    return mu**i/math.factorial(i)*math.e**(-mu)
+    return poisson.pmf(i,mu=mu)
 
 def Sigma_diag(s,i,Q,n,max):
     x=kapa12(s[i],max)
@@ -233,30 +222,14 @@ def Cashstat(x,s):
     return C
 
 def p_value_chi(x,df): #Alg.1
-    return scipy.stats.chi2.sf(x,df)
+    return chi2.sf(x,df)
 
 def KMtest(x,s):  #Alg.2a
     return p_value_norm(x,Max_mean(s),math.sqrt(Max_var(s)))
 
-def bootstrap_asymptotic(Cmin,beta,n,snull,B=1000):  #Alg.2b
-    C = np.zeros(B)
-    s=generate_s(n,beta,snull)
-    for i in range(B):
-        x = poisson_data(s)
-        if np.all(np.abs(x) < 1e-5):
-            continue
-        if snull=='constant':
-            bound=[[1e-5,30]]
-        else:
-            bound=[[1e-5,30], [-50, 10]]
-        xopt = opt.minimize(LLF, beta, args=(x, snull), bounds=bound)
-        r = generate_s(n, xopt['x'], snull)
-        C[i]=Cashstat(x,r)
-    return p_value_norm(Cmin, statistics.mean(C), statistics.stdev(C))
-
 def uncon_theory_test(x,beta,n,snull):  #Alg.3a
     s=generate_s(n,beta,snull)
-    I=np.mat([1.for i in range(len(beta))]).T
+    I=np.mat([1.0 for i in range(len(beta))]).T
     X=design_mat(beta,n,snull)
     m=int(max(s))+1
     if m<=10:
@@ -276,6 +249,19 @@ def con_theory_test(x,beta,n,snull):  #Alg.3b
         maximum=2*m
     return p_value_norm(x,expectation(s,n,X,I,maximum),math.sqrt(Var(s,n,X,I,maximum)))
 
+def bootstrap_asymptotic(Cmin,beta,n,snull,B=1000):  #Alg.2b
+    C = np.zeros(B)
+    s=generate_s(n,beta,snull)
+    for i in range(B):
+        x = poisson_data(s)
+        if np.all(np.abs(x) < 1e-5):
+            continue
+        bound = [[1e-5, 30]] if snull == 'constant' else [[1e-5, 30], [-50, 10]]
+        xopt = opt.minimize(LLF, beta, args=(x, snull), bounds=bound, method="L-BFGS-B")
+        r = generate_s(n, xopt['x'], snull)
+        C[i]=Cashstat(x,r)
+    return p_value_norm(Cmin, statistics.mean(C), statistics.stdev(C))
+
 def bootstrap_test(Cmin,beta,n,snull,B=1000):  #Alg.4a
     C = np.zeros(B)
     s=generate_s(n,beta,snull)
@@ -283,18 +269,11 @@ def bootstrap_test(Cmin,beta,n,snull,B=1000):  #Alg.4a
         x = poisson_data(s)
         if np.all(np.abs(x) < 1e-5):
             continue
-        if snull=='constant':
-            bound=[[1e-5,30]]
-        else:
-            bound=[[1e-5,30], [-50, 10]]
-        xopt = opt.minimize(LLF, beta, args=(x, snull), bounds=bound)
+        bound = [[1e-5, 30]] if snull == 'constant' else [[1e-5, 30], [-50, 10]]
+        xopt = opt.minimize(LLF, beta, args=(x, snull), bounds=bound, method="L-BFGS-B")
         r = generate_s(n, xopt['x'], snull)
         C[i]=Cashstat(x,r)
-    k=0
-    for i in range(B):
-        if C[i]>=Cmin:
-            k+=1
-    return k/B
+    return np.mean(C >= Cmin)
 
 def bootstrap_bias(Cmin,beta,n,snull,B1=1000,B2=1000):  #Alg.4b
     theta_tilde=np.zeros(len(beta))
@@ -303,11 +282,8 @@ def bootstrap_bias(Cmin,beta,n,snull,B1=1000,B2=1000):  #Alg.4b
         x = poisson_data(s)
         if np.all(np.abs(x) < 1e-5):
             continue
-        if snull == 'constant':
-            bound = [[1e-5, 30]]
-        else:
-            bound = [[1e-5, 30], [-50, 10]]
-        xopt = opt.minimize(LLF, beta, args=(x, snull), bounds=bound)
+        bound = [[1e-5, 30]] if snull == 'constant' else [[1e-5, 30], [-50, 10]]
+        xopt = opt.minimize(LLF, beta, args=(x, snull), bounds=bound, method="L-BFGS-B")
         theta_tilde += xopt['x']
     theta_adj=2*beta-theta_tilde/B1
     if theta_adj[0]<1e-5:
@@ -322,24 +298,13 @@ def double_boostrap(Cmin,beta,n,snull,B1=1000,B2=1000):  #Alg.4c
         x = poisson_data(s)
         if np.all(np.abs(x) < 1e-5):
             continue
-        if snull == 'constant':
-            bound = [[1e-5, 30]]
-        else:
-            bound = [[1e-5, 30], [-50, 10]]
-        xopt = opt.minimize(LLF, beta, args=(x, snull), bounds=bound)
+        bound = [[1e-5, 30]] if snull == 'constant' else [[1e-5, 30], [-50, 10]]
+        xopt = opt.minimize(LLF, beta, args=(x, snull), bounds=bound, method="L-BFGS-B")
         r = generate_s(n, xopt['x'], snull)
         C[i] = Cashstat(x, r)
         pvalue[i]=bootstrap_test(C[i],xopt['x'],n,snull,B2)
-    k = 0
-    for i in range(B1):
-        if C[i] >= Cmin:
-            k += 1
-    p=k/B1
-    l=0
-    for i in range(B1):
-        if pvalue[i]<=p:
-            l+=1
-    return l/B1
+    p = np.mean(C >= Cmin)
+    return np.mean(pvalue <= p)
 
 def LLF(theta,x,snull):
     if snull!='constant' and snull!='powerlaw' and snull!='exp':
@@ -347,10 +312,7 @@ def LLF(theta,x,snull):
         return 0
     n = len(x)
     s = generate_s(n,theta,snull)
-    value=0
-    for i in range(n):
-        value+=x[i]*math.log(s[i],math.e)-s[i]
-    return -value
+    return sum(s-x*np.log(s))
 
 def design_mat(beta,n,snull):
     p=len(beta)
@@ -368,16 +330,13 @@ def single_test(n,B,B1,B2,beta,strue,snull,alpha,iters):
     s=generate_s(n,beta,strue) # ground-truth
     rejections=np.zeros(8) #totally 8 methods, if strue=snull: Type I Error; if strue!=snull: Power
     for i in range(iters): # repetition
-        print(i)
+        print(f"Iteration {i + 1}")
         x=poisson_data(s)
         if np.all(np.abs(x) < 1e-5): # x==0, always accept
             continue
 
-        if snull == 'constant':
-            bound = [[1e-5, 30]]
-        else:
-            bound = [[1e-5, 30], [-50, 10]]
-        xopt = opt.minimize(LLF, beta, args=(x, snull), bounds=bound) # Get MLE
+        bound = [[1e-5, 30]] if snull == 'constant' else [[1e-5, 30], [-50, 10]]
+        xopt = opt.minimize(LLF, beta, args=(x, snull), bounds=bound, method="L-BFGS-B") # Get MLE
         betahat=xopt['x']
         r = generate_s(n, betahat, snull) # null-distribution
         Cmin=Cashstat(x,r)
@@ -397,7 +356,7 @@ def single_test(n,B,B1,B2,beta,strue,snull,alpha,iters):
 
     return rejections/iters     #Rejection Rate
 
-n=100  # number of bins
+n=10  # number of bins
 B=B1=B2=100  # bootstrap repetition times
 beta=np.array([3,3])  #ground-truth beta*
 strue='exp'  # true s
