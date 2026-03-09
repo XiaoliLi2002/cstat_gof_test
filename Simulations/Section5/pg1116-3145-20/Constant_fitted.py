d08d24f9b4bdcb4af8584e24251e1d4ed769da1a
Xiaoli Li <121785218+XiaoliLi2002@users.noreply.github.com>
2026-03-09 17:53:21 -0500

    Update Constant_fitted.py

--- Simulations/Section5/pg1116-3145-20/Constant_fitted.py
+++ Simulations/Section5/pg1116-3145-20/Constant_fitted.py
@@ -38,20 +38,22 @@ count=np.genfromtxt(path4figure1,skip_header=0)
 data=count[:,1]
 count=np.genfromtxt(path4figure2,skip_header=0)
 data2=count[:,1]
-ytick=[0,1,np.mean(data),2,np.mean(data2),3,4,5,6]
-ylabel=['0','0.249','1','1.497','2','3','4','5','6']
+print(np.mean(data),np.mean(data2))
+ytick=[0,np.mean(data),1,np.mean(data2),2,3,4,5,6]
+ylabel=['0','0.429','1','1.497','2','3','4','5','6']
 wavelength=[20.0+.0125/2+.0125*i for i in range(n)]
 xerror=0.0125
 fig,ax=plt.subplots(figsize=(8,6))
 #plt.errorbar(wavelength,data,xerr=xerror,yerr=data**0.5,alpha=0.3,color='r',label='NEW OBS SEG1')
-plt.scatter(wavelength,data,color='r',label='2018 OBS SEG1',s=6,marker='+')
+plt.scatter(wavelength,data,color='r',label='2018 OBS SEG1',s=6,marker='.',alpha=0.8)
 #plt.errorbar(wavelength,data2,xerr=xerror,yerr=data2**0.5,alpha=0.3,color='b',label='BACK NEW OBS SEG1')
-plt.scatter(wavelength,data2,color='b',label='BACK 2018 OBS SEG1',s=6,marker='+')
-plt.axhline(np.mean(data),alpha=1,color='r',linestyle='-')
-plt.axhline(np.mean(data2),alpha=1,color='b',linestyle='-')
+plt.scatter(wavelength,data2,color='b',label='BACK 2018 OBS SEG1',s=6,marker='d',alpha=0.8)
+plt.axhline(np.mean(data),alpha=0.8,color='r',linestyle='-')
+plt.axhline(np.mean(data2),alpha=0.8,color='b',linestyle='-')
 plt.grid(alpha=0.5)
 plt.xlabel('Wavelength (Ang.)')
 plt.ylabel('Counts')
+plt.ylim(-0.1,6.2)
 plt.xlim((wavelength[0]-xerror,wavelength[n-1]+xerror))
 plt.legend(fontsize=15)
 plt.yticks(ytick,ylabel)
